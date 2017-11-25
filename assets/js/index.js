c781a47198527fb53ff8739952ef0517d3ba2808
Matt D Smith <mds@mds.attlocal.net>
2017-11-25 13:05:53 -0500

    initial setup

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+
+  // $('.class').on("click", function (event) {
+  //   something();
+  // });
+
+  // function something(){
+  //   console.log("something");
+  // }
+
+  // event tracking
+  // $('.class').click(function(){ _gs('event', 'class was clicked') });
+
+});
